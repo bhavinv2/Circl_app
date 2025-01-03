c25223b6d1229c65737bea2e5ce6a53945347a13
Bhavin Vulli <vulli.bhavin@gmail.com>
2025-01-03 17:29:36 -0500

    Initial Commit

---
+++ Circl/CirclApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclApp.swift
+//  Circl
+//
+//  Created by Bhavin Vulli on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CirclApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CirclTests/CirclTests.swift
@@ -0,0 +1,17 @@
+//
+//  CirclTests.swift
+//  CirclTests
+//
+//  Created by Bhavin Vulli on 1/3/25.
+//
+
+import Testing
+@testable import Circl
+
+struct CirclTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
