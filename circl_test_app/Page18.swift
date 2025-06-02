784f9e87a343b5ad39b40613007a4fd14119c856
enderlutz <fragnedelgado1@gmail.com>
2025-06-02 14:59:08 -0500

    Update Page18.swift
    added design

--- circl_test_app/Page18.swift
+++ circl_test_app/Page18.swift
@@ -6,6 +6,81 @@ struct Page18: View {
             // Background Color
             Color(hexCode: "004aad")
                 .edgesIgnoringSafeArea(.all)
+            
+            ZStack {
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 120, height: 120)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 - 80), y: -UIScreen.main.bounds.height / 2 + 0)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 120, height: 120)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 - 130), y: -UIScreen.main.bounds.height / 2 + 0)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 100, height: 100)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 - 30), y: -UIScreen.main.bounds.height / 2 + 40)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 100, height: 100)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 - 110), y: -UIScreen.main.bounds.height / 2 + 50)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 100, height: 100)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 + 170), y: -UIScreen.main.bounds.height / 2 + 30)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 100, height: 100)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 + 210), y: -UIScreen.main.bounds.height / 2 + 60)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 80, height: 80)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 + 90), y: -UIScreen.main.bounds.height / 2 + 50)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 90, height: 90)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 + 50), y: -UIScreen.main.bounds.height / 2 + 30)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 110, height: 110)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 + 150), y: -UIScreen.main.bounds.height / 2 + 80)
+            }
+
+            // Bottom Left Cloud (Flipped from Bottom Right Cloud)
+            ZStack {
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 100, height: 100)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 - 30), y: UIScreen.main.bounds.height / 2 - 40)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 100, height: 100)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 - 100), y: UIScreen.main.bounds.height / 2 - 50)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 90, height: 90)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 - 50), y: UIScreen.main.bounds.height / 2 - 30)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 90, height: 90)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 - 40), y: UIScreen.main.bounds.height / 2 - 70)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 80, height: 80)
+                    .offset(x: -(UIScreen.main.bounds.width / 2 - 140), y: UIScreen.main.bounds.height / 2 - 30)
+            }
 
             VStack(spacing: 30) {
                 Spacer()
