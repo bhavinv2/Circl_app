53bd43a4c589d10eb7a1bcc65870d4b5ea3beb58
enderlutz <fragnedelgado1@gmail.com>
2025-06-02 10:43:22 -0500

    Update PageSkillSellingMatching.swift add links
    add links

--- circl_test_app/PageSkillSellingMatching.swift
+++ circl_test_app/PageSkillSellingMatching.swift
@@ -346,6 +346,10 @@ struct PageSkillSellingMatching: View {
                                 MenuItem(icon: "newspaper.fill", title: "News & Knowledge")
                             }
                             
+                            NavigationLink(destination: PageSkillSellingMatching().navigationBarBackButtonHidden(true)) {
+                                MenuItem(icon: "dollarsign.circle.fill", title: "The Circl Exchange")
+                            }
+                            
                             Divider()
                             
                             NavigationLink(destination: PageCircles().navigationBarBackButtonHidden(true)) {
