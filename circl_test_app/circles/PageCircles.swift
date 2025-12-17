d28cf5ba8ed0df1675110b091c591626e4c13f0e
Fragne Delgado <fjdelga3@cougarnet.uh.edu>
2025-12-17 00:09:04 -0600

    I think this might be the final push with mac version

--- circl_test_app/circles/PageCircles.swift
+++ circl_test_app/circles/PageCircles.swift
@@ -246,7 +246,11 @@ struct PageCircles: View {
                         }
                         .padding(.horizontal, 18)
                     }
+                    #if targetEnvironment(macCatalyst)
+                    .padding(.top, 20)
+                    #else
                     .padding(.top, UIDevice.current.userInterfaceIdiom == .pad ? 16 : 8)
+                    #endif
                     .padding(.bottom, 6)
                     
                     // MARK: Enhanced Circle List
